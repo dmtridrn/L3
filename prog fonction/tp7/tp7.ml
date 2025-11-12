de25c40b2bfc55ae96293ecc77c130f5e98dd3bb
Dimitri Dron <dimitri.dron-jeannequin@etu.u-paris.fr>
2025-11-12 09:59:07 +0100

    a

--- prog fonction/tp7/tp7.ml
+++ prog fonction/tp7/tp7.ml
@@ -1,5 +1,5 @@
 type transitions = int -> (int list)
-type graph = {size : int;fwd : transitions}
+type graph = {size : int; fwd : transitions}
 
 let f x = match x with
   0 -> [2]
@@ -23,3 +23,28 @@ let atteints f k x =
       | a::reste -> if not (List.mem a acc) then Some a else None 
     in aux fonc (k-1) next acc 
   in aux f k x [];;
+
+let accessible f x y = 
+  let rec aux vus s = 
+    if s = y then true else
+    if List.mem s vus then false else
+      List.exists (fun v -> aux (s::vus) v) (f s)
+    in aux [] x;;
+
+let modify g x l =
+  let new_fwd y =
+    if y = x then l
+    else g.fwd y
+  in
+  {size = g.size; fwd = new_fwd};;
+
+let un_completion g =
+  let new_fwd p =
+    let direct = g.fwd p in
+    let indirect =
+      List.flatten (List.map (fun q -> g.fwd q) direct)
+    in
+    List.sort_uniq compare (direct @ indirect)
+  in
+  {size = g.size; fwd = new_fwd}
+
