3d6e4ea3128b8dcb4c8cd3fe5d9686ae78fe03ba
Dron Dimitri <dimitri.dron-jeannequin@etu.u-paris.fr>
2026-01-27 23:15:38 +0100

    a

--- python/tp/tp1/hello.py
+++ python/tp/tp1/hello.py
@@ -1,3 +1,5 @@
+from time import process_time
+
 def count_blanks(s):
     cpt = 0
     for c in s:
@@ -8,15 +10,37 @@ def count_blanks(s):
 def is_palindrome(s):
     return s == s[::-1]
 
-def count(s,o):
-    lon = len(o)
-    i = 0
+def count(s, o):
+    if not o:
+        return 0
     cpt = 0
-    while i < len(s)-len(o)+1:
-        if s[i:i+lon] == o:
-            cpt+=1
-            i+=lon
-        else:
-            i+=1
+    i = 0
+    while i < len(s):
+        pos = s.find(o, i)
+        if pos == -1:
+            break
+        cpt += 1
+        i = pos + len(o)
     return cpt
 
+debut = process_time()
+
+count(1000000*"aaaaajjajjaaajjjjjj", "jj")
+
+
+fin = process_time()
+
+temps = fin - debut
+
+print(f"Temps CPU utilisé pour la mienne: {temps:.4f} secondes")
+
+debut = process_time()
+
+str.count(100000*"aaaaajjajjaaajjjjjj", "jj")
+
+
+fin = process_time()
+
+temps = fin - debut
+
+print(f"Temps CPU utilisé pour str: {temps:.4f} secondes")
