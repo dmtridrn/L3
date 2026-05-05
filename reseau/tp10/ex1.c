c27e26c4f64ea93ef40edf5b0876c78cd7b374df
DRON JEANNEQUIN dimitri <dimitri.dron-jeannequin@etu.u-paris.fr>
2026-05-05 14:08:20 +0200

    a

---
+++ reseau/tp10/ex1.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include <net/if.h>
+
+int main(){
+    
+}
