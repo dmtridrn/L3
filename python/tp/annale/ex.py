8fc5d10fb626aef7566bc8780347b5cfd30536b9
DRON JEANNEQUIN dimitri <dimitri.dron-jeannequin@etu.u-paris.fr>
2026-05-06 15:26:56 +0200

    a

--- python/tp/annale/ex.py
+++ python/tp/annale/ex.py
@@ -1,6 +1,7 @@
 import re
 from pathlib import Path
 import shutil
+import random
 
 def prefixes(x):
     return [x[:i] for i in range(len(x)+1)]
@@ -31,7 +32,8 @@ date_regex = re.compile(date_format)
 mobj = date_regex.fullmatch(date_str)
 jour = mobj.group(1)
 zon = mobj.group(7)
-print(f"{jour}, {zon}")
+#print(f"{jour}, {zon}")
+
 
 def top10(nomf):
     d = dict()
@@ -52,7 +54,51 @@ def recent_txt(nomdir, n):
     path = Path(nomdir)
     res = []
     for i in path.rglob("*.txt"):
-        res.append(i)
+        if(i.is_file()):
+            res.append(i)
     return sorted(res, key=lambda x: x.stat().st_mtime, reverse=True)[:n]
 
-print(recent_txt(".", 10))
+class tournoi:
+    
+    def __init__(self, L):
+        self.statut = dict()
+        for i in L:
+            self.statut[i] = self.statut.get(i,'D')
+        self.res = dict()
+        self.encours = []
+
+    def nouv_match(self):
+        dispo = set()
+        #on get toutes les équipes dispo
+        for i,j in self.statut.items():
+            if j == 'D' and len(self.res[e1]) < len(self.statut)-1: #on ajoute que s'ils ont pas fait tous leurs match
+                dispo.add(i)
+        #on en récup 2
+        e1 = random.choice(dispo)
+        dispo.pop(e1)
+        for i,j in self.res[e1]:
+            dispo.pop(i) #on vire ceux que e1 a déjà joué
+        e2 = random.choice(dispo)
+        #ils jouent
+        self.statut[e1] = 'J'
+        self.statut[e2] = 'J'
+        self.encours.append(e1,e2)
+        print(f"Nouveau match: {e1} - {e2}")
+
+    def resultat(self, e1, e2):
+        for i in range(len(self.encours)):
+            if self.encours[i] == (e1,e2) or self.encours[i] == (e2,e1):
+                self.encours.pop(i)
+                self.statut[e1] = 'D' #le gagnant est dispo
+                self.res[e1].append((e2, True))
+                self.res[e2].append((e1, False))
+                derniers_match = list(self.res[e2])[-3:]
+                for j,k in derniers_match:
+                    if k == True:
+                        self.statut[e2] = 'D' #il a win un match sur ses trois derniers
+                        return
+                self.statut[e2] = 'E'
+                return
+        print("Veuillez rentrer un match valide")
+    
+    
